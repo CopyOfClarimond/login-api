a4138a2f58aebebb1462567fab7fee1079342e41
CopyOfClarimond <berko.itay@gmail.com>
2024-07-31 22:36:36 +0545

    Update server.js

--- server.js
+++ server.js
@@ -7,18 +7,22 @@ app.use(express.json());
 
 app.all("/api/*", (req, res) => {
     const url = `https://discord.com/api/v9${req.url.replace("/api/", "/")}`;
-    
+
     console.log(`Forwarding request to: ${url}`);
     console.log(`Request body: ${JSON.stringify(req.body)}`);
 
     req.pipe(request({ url, method: req.method, json: req.body }))
         .on('error', (err) => {
             console.error('Request error:', err);
-            res.status(500).send('An error occurred');
+            if (!res.headersSent) {
+                res.status(500).json({ error: 'An error occurred' });
+            }
+        })
+        .on('response', (proxiedRes) => {
+            proxiedRes.headers['content-type'] = 'application/json';
         })
         .pipe(res);
 });
-
 app.listen(port, () => {
     console.log(`Proxy server running at http://localhost:${port}`);
 });
