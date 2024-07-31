915c618ce020bc58f3e9d760ff9832362ac1e183
CopyOfClarimond <berko.itay@gmail.com>
2024-07-31 22:13:43 +0545

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require("express");
-
+const request = require("request");
 const app = express();
-app.get("/", (req, res) => {
-	res.json({
-		"working?": true,
-	});
+const port = 3000;
+
+app.use(express.json());
+
+app.all("/api/*", (req, res) => {
+    const url = `https://discord.com/api/v9${req.url.replace("/api/", "/")}`;
+    
+    console.log(`Forwarding request to: ${url}`);
+    console.log(`Request body: ${JSON.stringify(req.body)}`);
+
+    req.pipe(request({ url, method: req.method, json: req.body }))
+        .on('error', (err) => {
+            console.error('Request error:', err);
+            res.status(500).send('An error occurred');
+        })
+        .pipe(res);
 });
 
-app.listen(3000, () => {
-	console.log("Listening to 3000");
+app.listen(port, () => {
+    console.log(`Proxy server running at http://localhost:${port}`);
 });
